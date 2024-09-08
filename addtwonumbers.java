0f73e56d23d335ac97eef82a6c34945ed1904475
Prem Shandilya <you@example.com>
2024-09-08 23:44:59 +0530

    added a code to add two numbers

---
+++ addtwonumbers.java
@@ -0,0 +1,9 @@
+class addtwonumbers{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 12;
+        int c = a+b;
+
+        System.out.println("The sum of two numbers are:"+c);
+    }
+}
