1bee90953184991b24536aa891d250db619eb2da
Prem Shandilya <you@example.com>
2024-09-09 13:18:50 +0530

    added a code to find odd or even number.

---
+++ oddeven.java
@@ -0,0 +1,10 @@
+public class oddeven {
+    public static void main(String[] args) {
+        int num = 5;
+        if(num%2 ==0)
+        System.out.println(num+" is even");
+        else 
+        System.out.println(num+" is odd");
+    }
+    
+}
