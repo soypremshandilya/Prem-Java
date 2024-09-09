dd38cd15ee25cc216f4ee397c9ca246d4af1678d
Prem Shandilya <you@example.com>
2024-09-09 13:14:12 +0530

    corrected code

--- MaxOfThreeNumbers.java
+++ MaxOfThreeNumbers.java
@@ -1,13 +1,9 @@
 public class MaxOfThreeNumbers {
 
     public static void main(String[] args) {
-   
-    int num1 = 10, num2 = 20, num3 = 15, max;
-   
-    max = (num1 > num2) ? (num1 > num3 ? num1 : num3) : (num2 > num3 ? num2 : num3);
-   
-    System.out.println("Maximum of " + num1 + ", " + num2 + ", and " + num3 + " is: " + max);
-   
+    int a = 1, b = 2, c = 3;
+    int max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+    System.out.println("The highest number is"+max);
     }
-   
    }
+
